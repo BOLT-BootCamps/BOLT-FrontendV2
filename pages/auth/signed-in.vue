a689e2948fe0d1344c630c0acb045d0c5eeff37d
Zhehai <zhehaizhang1@gmail.com>
2021-11-10 19:43:22 -0500

    continuous delivery and tailwind

--- pages/auth/signed-in.vue
+++ pages/auth/signed-in.vue
@@ -1,3 +1,23 @@
 <template>
-  <p>Signing in...</p>
+  <div>
+    <p>Signing in...</p>
+    <p v-if="message !== ''">{{message}}</p>
+
+  </div>
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  mounted() {
+    if (this.$route.hash !== "") {
+      this.message = this.$route.hash
+    }
+  }
+}
+</script>
