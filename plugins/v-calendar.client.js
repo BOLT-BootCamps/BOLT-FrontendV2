648981483ad4f52d55af0140aa72cd5cefe6beb7
Zhehai <zhehaizhang1@gmail.com>
2022-04-02 08:51:44 -0400

    calendar

--- plugins/v-calendar.client.js
+++ plugins/v-calendar.client.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+import Calendar from 'v-calendar/lib/components/calendar.common'
 
 Vue.component('DatePicker', DatePicker)
+Vue.component('VCalendar', Calendar)
