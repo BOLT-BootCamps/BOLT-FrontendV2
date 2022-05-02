d49078596825598a32c99c04e68cc3dacd74f638
Zhehai <zhehaizhang1@gmail.com>
2022-05-01 23:00:31 -0400

    scatter plot

---
+++ components/ScatterChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Scatter } from 'vue-chartjs'
+
+export default {
+  extends: Scatter,
+  props: ['data', 'options'],
+  data () {
+    return {
+      scatterChartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        tooltips: {
+          backgroundColor: '#17BF62'
+        },
+        scales: {
+        }
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(this.data, { ...this.scatterChartOptions, ...this.options })
+  }
+}
+</script>
